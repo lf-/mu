f0e82f482915671526abeb2b1fb4707cf245bcac
Jade <software@lfcode.ca>
2021-04-21 06:18:34 -0700

    make it easier to build by using system clang

--- kern/build.rs
+++ kern/build.rs
@@ -3,11 +3,12 @@ use build_bits::external_dep;
 fn main() {
     let mut builder = cc::Build::new();
     builder
-        .compiler("/opt/llvm/bin/clang")
+        .compiler("clang")
         .no_default_flags(true) // since this is asm the default flags just screw us up
         .flag("--target=riscv64-none-unknown-elf") // clang uses different triples than rustc, lol
         .flag("-march=rv64imac")
         .flag("-mno-relax");
+
     // some weird linker error:
     // ld.lld: error: out/libinit.a(init.o):(.text+0x0): relocation
     // R_RISCV_ALIGN requires unimplemented linker relaxation; recompile
@@ -15,6 +16,7 @@ fn main() {
 
     builder.clone().file("src/trampoline.s").compile("kern_asm");
 
+    external_dep("build.rs");
     external_dep("src/trampoline.s");
     external_dep("kern.ld");
 }
--- shoo/build.rs
+++ shoo/build.rs
@@ -3,11 +3,12 @@ use build_bits::external_dep;
 fn main() {
     let mut builder = cc::Build::new();
     builder
-        .compiler("/opt/llvm/bin/clang")
+        .compiler("clang")
         .no_default_flags(true) // since this is asm the default flags just screw us up
         .flag("--target=riscv64-none-unknown-elf") // clang uses different triples than rustc, lol
         .flag("-march=rv64imac")
         .flag("-mno-relax");
+
     // some weird linker error:
     // ld.lld: error: out/libinit.a(init.o):(.text+0x0): relocation
     // R_RISCV_ALIGN requires unimplemented linker relaxation; recompile
@@ -19,6 +20,7 @@ fn main() {
         .file("src/vectors.s")
         .compile("shoo_asm");
 
+    external_dep("build.rs");
     external_dep("src/init.s");
     external_dep("src/vectors.s");
     external_dep("shoo.ld");
